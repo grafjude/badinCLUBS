82037be1f1bbc7b7f670cecd19b7eb7cbbcd7953
JG <grafjude@badinhs.org>
2017-01-04 10:32:07 -0500

    new view controller

---
+++ badinCLUBS/ClubDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ClubDetailViewController.swift
+//  badinCLUBS
+//
+//  Created by BHSRam8 on 1/4/17.
+//  Copyright © 2017 Jude Graf. All rights reserved.
+//
+
+import UIKit
+
+class ClubDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBOutlet var clubLabelView:UILabel!
+    var clubLabel = ""
+    
+ 
+}
--- badinCLUBS/ViewController.swift
+++ badinCLUBS/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var badinClubs = ["club 1", "club 2", "club 3", "club 4", "club 5", "club 6","club 7","club 8","club 9", "club 10" , "club 11"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return badinClubs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier="Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = badinClubs[indexPath.row]
+        return cell
+    }
+    
 }
 
