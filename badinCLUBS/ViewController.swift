ba4a8cb0c5f97c2be4871010a649810c06bb324d
JG <grafjude@badinhs.org>
2017-01-04 07:46:10 -0500

    Initial Commit

---
+++ badinCLUBS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  badinCLUBS
+//
+//  Created by BHSRam8 on 1/4/17.
+//  Copyright © 2017 Jude Graf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
