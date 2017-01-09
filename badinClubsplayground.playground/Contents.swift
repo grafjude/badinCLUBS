fed4ff775e77b788874921359b8e033af929edf4
JG <grafjude@badinhs.org>
2017-01-09 02:41:04 -0500

    playground

---
+++ badinClubsplayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//Jude Graf's playground
+
+//arrays
+
+var clubNames = ["Badin Studio Club", "Civics Day Club", "Creative Writing Club", "Culinary Club", "DECA", "French Club", "INTERalliance", "JSA", "Spanish Club", "Spirit Club", "Student Council"]
+
+var clubLeaders = ["Teacher: Mrs. Daniels", "Teachers: Mr. Mears and Mr. Keating", "Teacher: Mr. O'hara", "Teachers: Mr. Gretz and Mrs. Bierbaum", "Teachers: Mr. DeAngelo & Mr. McCabe", "Teacher: Mrs. McAdams", "Teachers: Mr. Memory & Mr. Gretz", "Teacher: Mr. Keating", "Teacher: Mrs. Oliver", "Teacher: Mrs. Suedkamp", "Teachere: Mrs. Keating"]
+
+var clubFunctions = [
+    "Badin Studio club is an organization that pubish's students artwork at Badin Highschool. ",
+    "Civics day club was made to promote civics day at Badin Highschool.", "Creative writing is for students who want to express their feelings through writing. If you like writing, this is the club for you.",
+    "Culinary club is for students who like food. If cooking or eating is your euphoria, you will love culinary club.",
+    "DECA is the club for business students. In DECA, students compete all over the country in business competitions. Winners move on to higher competition. ",
+    "French Club is for the students who want to learn about French culture or eat French foods.",
+    "INTERalliance is a club organized in Cincinnati. It is for highschool tech students who want to reach out to local businesses for possible internships.",
+    "JSA is a club made for students who like to argue. It's a debate club for students to argue current issues.",
+    "Spanish Club is for the students who want to learn about Spanish culture or eat Spanish foods. ",
+    "Spirit Club is for the students at Badin to be in charge of all school spirit related activities.", "Student Cuncil is the organization of students at Badin Highschool that holds the council. Such as President/Vice President."]
+
+var clubImages = ["artstudio.jpg", "civicsday.jpg", "writing.jpg", "culinary.jpg", "Deca.jpg", "frenchclub.jpg", "Interalliance.jpg", "JSA.jpg", "spanishclub.jpg", "spiritclub.jpg", "studentcouncil.jpg"]
+
+//example of an append
+clubImages.append("anyclubimageneeded")
+
+//math function
+func addTwo (num1 : Int, num2 : Int) {
+    print(num1 + num2)
+}
+addTwo(num1:5, num2:10)
+addTwo(num1:12, num2:45)
+
+//app doesnt need a math function so put it in a playground
